4246208a4c220bab5f11e99123e7a81938325434
Arshil Pansuiya <2102030400206@silveroakuni.ac.in>
2023-11-07 11:52:03 +0530

    env upated

--- config/database.js
+++ config/database.js
@@ -101,23 +101,20 @@
 // };
 
 // strapi-api/config/database.js
-module.exports = ({ env }) => {
-  return {
-    defaultConnection: "default",
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "Arshil"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "arshil"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not required
-        // ssl: {
-        //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-        // },
-      },
-      debug: false,
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "Arshil"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "arshil"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      // },
     },
-  };
-};
+    debug: false,
+  },
+});
